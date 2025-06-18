fd6178d5f562af5e673768b50b1dcb1e835c2680
Alireza <a.z.nikravesh@gmail.comm>
2025-06-18 13:42:03 +0330

    added tag entity

---
+++ Dev-Hub/src/01.Core/Dev-Hub.Core.Domain/Tags/Entities/Tag.cs
@@ -0,0 +1,20 @@
+﻿using Dev_Hub.Core.Domain.Framework.Entities;
+
+namespace Dev_Hub.Core.Domain.Tags.Entities;
+public class Tag : BaseEntity
+{
+    public string Title { get; private set; }
+
+    private Tag()
+    {
+    }
+
+    private Tag(string title)
+    {
+        Title = title;
+    }
+
+    public static Tag Create(string title) => new(title);
+
+    public void Update(string title) => Title = title;
+}
