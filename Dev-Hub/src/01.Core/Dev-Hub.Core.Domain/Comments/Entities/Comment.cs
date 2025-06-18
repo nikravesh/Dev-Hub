d6c8cf99904de824f34dbffb00dd2bb5224fcebb
Alireza <a.z.nikravesh@gmail.comm>
2025-06-18 13:48:19 +0330

    added comment

---
+++ Dev-Hub/src/01.Core/Dev-Hub.Core.Domain/Comments/Entities/Comment.cs
@@ -0,0 +1,37 @@
+﻿using Dev_Hub.Core.Domain.Framework.Entities;
+
+namespace Dev_Hub.Core.Domain.Comments.Entities;
+public class Comment : BaseEntity
+{
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public string Comments { get; private set; }
+    public DateOnly CommentDate { get; private set; }
+    public TimeOnly CommentTime { get; private set; }
+
+    private Comment() { }
+
+    private Comment(
+        string firstName,
+        string lastName,
+        string comment,
+        DateOnly commentDate,
+        TimeOnly commentTime)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Comments = comment;
+        CommentDate = commentDate;
+        CommentTime = commentTime;
+    }
+
+    public static Comment Create(
+        string firstName,
+        string lastName,
+        string comment,
+        DateOnly commentDate,
+        TimeOnly commentTime)
+        => new(firstName, lastName, comment, commentDate, commentTime);
+
+    public void Update(string comment) => Comments = comment;
+}
