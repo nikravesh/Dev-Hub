b0c02aed6723e7d6662df29309f7e1d628da5bd8
Alireza <a.z.nikravesh@gmail.comm>
2025-06-18 13:25:01 +0330

    added base entity and content

---
+++ Dev-Hub/src/01.Core/Dev-Hub.Core.Domain/Contents/Entities/Content.cs
@@ -0,0 +1,33 @@
+﻿using Dev_Hub.Core.Domain.Framework.Entities;
+
+namespace Dev_Hub.Core.Domain.Contents.Entities;
+public class Content : BaseEntity
+{
+    public string Title { get; private set; }
+    public string Description { get; private set; }
+    public DateOnly PublishDate { get; private set; }
+    public TimeOnly PublishTime { get; private set; }
+    public int AuthorName { get; private set; }
+
+    public Content()
+    {
+    }
+
+    private Content(string title, string description, DateOnly publishDate, TimeOnly publishTime, int authorName)
+    {
+        Title = title;
+        Description = description;
+        PublishDate = publishDate;
+        PublishTime = publishTime;
+        AuthorName = authorName;
+    }
+
+    public static Content Create(string title, string description, DateOnly publishDate, TimeOnly publishTime, int authorName)
+    => new(title, description, publishDate, publishTime, authorName);
+
+    public void Update(string title,string description)
+    {
+        Title = title;
+        Description = description;
+    }
+}
---
+++ Dev-Hub/src/01.Core/Dev-Hub.Core.Domain/Framework/Entities/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Dev_Hub.Core.Domain.Framework.Entities;
+public abstract class BaseEntity
+{
+    public Guid BusinessId { get; private set; }
+    public long Id { get; private set; }
+
+    protected BaseEntity()
+    {
+        BusinessId = Guid.NewGuid();
+    }
+}
