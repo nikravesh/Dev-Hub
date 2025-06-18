88ed4d4ba91939086b66bcf08391e15c03f2b112
Alireza <a.z.nikravesh@gmail.comm>
2025-06-18 13:37:19 +0330

    added author entity

---
+++ Dev-Hub/src/01.Core/Dev-Hub.Core.Domain/Authors/Entities/Author.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+
+using Dev_Hub.Core.Domain.Framework.Entities;
+
+namespace Dev_Hub.Core.Domain.Authors.Entities;
+public class Author : BaseEntity
+{
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+
+    private Author() { }
+
+    private Author(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+
+    public static Author Create(string firstName, string lastName)
+    => new(firstName, lastName);
+}
