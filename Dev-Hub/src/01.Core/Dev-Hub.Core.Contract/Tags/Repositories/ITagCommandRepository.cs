37f94e2184ecf87d7d4c25170b448b24cf0b958e
Alireza <a.z.nikravesh@gmail.comm>
2025-06-23 10:14:32 +0330

    added tag repository

---
+++ Dev-Hub/src/01.Core/Dev-Hub.Core.Contract/Tags/Repositories/ITagCommandRepository.cs
@@ -0,0 +1,5 @@
+﻿using Dev_Hub.Core.Contract.Common.Repositories;
+using Dev_Hub.Core.Domain.Tags.Entities;
+
+namespace Dev_Hub.Core.Contract.Tags.Repositories;
+public interface ITagCommandRepository : IBaseRepository<Tag, long>;
