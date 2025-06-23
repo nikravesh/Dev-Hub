a4932177d6550eddd78a89071c1c01e4c170fa3d
Alireza <a.z.nikravesh@gmail.comm>
2025-06-23 09:51:38 +0330

    added base generic repository

---
+++ Dev-Hub/src/01.Core/Dev-Hub.Core.Contract/Common/Repositories/IBaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace Dev_Hub.Core.Contract.Common.Repositories;
+public interface IBaseRepository<TEntity, TId>
+{
+    void Delete(TId id);
+    void Delete(TEntity entity);
+    Task DeleteAsync(TId id);
+    TEntity? GetById(TId id);
+    Task<TEntity?> GetByIdAsync(TId id);
+    void Insert(TEntity entity);
+    Task InsertAsync(TEntity entity);
+    Task<IEnumerable<TEntity>> SelectAllAsync();
+    void Update(TEntity entity);
+    bool Exists(Expression<Func<TEntity, bool>> expression);
+    Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> expression);
+}
