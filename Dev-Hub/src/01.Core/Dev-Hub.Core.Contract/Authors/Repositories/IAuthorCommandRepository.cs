e4490dbdad2d399ee6e3178305a2227b559d7f19
Alireza <a.z.nikravesh@gmail.comm>
2025-06-23 10:01:21 +0330

    added author repository

---
+++ Dev-Hub/src/01.Core/Dev-Hub.Core.Contract/Authors/Repositories/IAuthorCommandRepository.cs
@@ -0,0 +1,7 @@
+﻿using Dev_Hub.Core.Contract.Common.Repositories;
+using Dev_Hub.Core.Domain.Authors.Entities;
+
+namespace Dev_Hub.Core.Contract.Authors.Repositories;
+public interface IAuthorCommandRepository : IBaseRepository<Author,long>
+{
+}
---
+++ Dev-Hub/src/02.Infra/Data/Dev-Hub.Infra.Data.Command.Sql/Authors/Repositories/AuthorCommandRepository.cs
@@ -0,0 +1,11 @@
+﻿using Dev_Hub.Core.Domain.Authors.Entities;
+using Dev_Hub.Infra.Data.Command.Sql.Common.Repositories;
+using Dev_Hub.Infra.Data.Command.Sql.Data;
+
+namespace Dev_Hub.Infra.Data.Command.Sql.Authors.Repositories;
+public class AuthorCommandRepository : BaseRepository<Dev_HubDbContext, Author, long>
+{
+    public AuthorCommandRepository(Dev_HubDbContext dbContext) : base(dbContext)
+    {
+    }
+}
